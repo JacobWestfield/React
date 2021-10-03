b6219df9795ef57b0771848abb5147b2af1d3e4a
JacobWestfield <91150870+JacobWestfield@users.noreply.github.com>
2021-10-03 07:14:19 +0300

    Add files via upload

---
+++ lesson4/src/components/Home/Home.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Grid, Box } from '@material-ui/core';
+
+
+
+
+function Home() {
+
+
+
+    return (
+        <Grid container>
+            <Grid item xs={12}>
+                <Box sx={{
+                    height: '100vh',
+                    backgroundColor: 'rgb(223, 141, 116)',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    flexDirection: 'column'
+                }}>
+                    <img src='logo512.png' alt='react' />
+                    <h1>Home Page</h1>
+                </Box>
+            </Grid>
+        </Grid>
+    )
+}
+
+
+export default Home;
