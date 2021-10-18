101a55bf5f83fe2c6a65f455fa0ec7eecfd9e413
JacobWestfield <91150870+JacobWestfield@users.noreply.github.com>
2021-10-18 15:40:44 +0300

    Delete selectors.js

--- selectors.js
+++
@@ -1,5 +0,0 @@
-export const getMessage = (store) => store.messages;
-
-export const getMessageList = (store) => getMessage(store)?.messageList || [];
-
-export const getChatMessage = (chatId) => (store) => getMessageList(store)[chatId];
